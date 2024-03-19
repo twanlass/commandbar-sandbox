424848e66fd07b03694f15190ad43d24342cb890
Tyler Wanlass <hello@tdub.co>
2024-03-19 13:23:54 -0700

    Adds stub pages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon()],
   output: "server",
   adapter: netlify()
 });
